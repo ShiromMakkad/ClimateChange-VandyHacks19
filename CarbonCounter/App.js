46cc9d66f0b9844852d9288537dce92e4ea282aa
Shirom Makkad <shirmak11@gmail.com>
2019-11-08 20:51:12 -0500

    Switched from Ant Design to react-native-elements and fnished opening screen

--- CarbonCounter/App.js
+++ CarbonCounter/App.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import { View, Text, Image, Platform, StatusBar } from 'react-native';
-import { Button } from '@ant-design/react-native';
-import { createAppContainer, SafeAreaView } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {View, Platform, StatusBar, ScrollView } from 'react-native';
+import {Button, Text, Image, ThemeProvider, Slider, Card, Icon } from 'react-native-elements';
+import {createAppContainer, SafeAreaView} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 if (Platform.OS === 'android') {
     SafeAreaView.setStatusBarHeight(0);
 }
 
+const theme = {
+    colors: {
+        primary: '#0B7310',
+    },
+    Slider: {
+        // maximumTrackTintColor: '#6C757D',
+        minimumTrackTintColor: '#6C757D',
+        thumbStyle: {
+            backgroundColor: '#0B7310',
+            width: 25,
+            height: 25,
+            borderRadius: 25 / 2,
+            padding: 9
+        },
+        trackStyle: {
+            height: 6,
+            borderRadius: 25 / 2
+        }
+    },
+    Card: {
+        containerStyle: {
+            borderRadius: 3
+        }
+    }
+};
+
 class App extends React.Component {
     static navigationOptions = {
         title: "Carbon Counter",
-        headerTitle:(<View style={{flexDirection: 'row', alignContent: 'center', justifyContent: 'center', padding: 5}}><Image style={{width: 50, height: 50}} source={require('./assets/icon.png')}/><Text style={{alignSelf: 'center', color: 'white', fontWeight: '500', fontSize: 22}}>   Carbon Counter</Text></View>),
+        headerTitle: (
+            <View style={{flexDirection: 'row', alignContent: 'center', justifyContent: 'center', padding: 5}}>
+                <Image style={{width: 50, height: 50}} source={require('./assets/icon.png')}/>
+                <Text style={{alignSelf: 'center', color: 'white', fontWeight: '500', fontSize: 22}}>  Carbon Counter</Text>
+            </View>),
         headerStyle: {
             backgroundColor: '#0B7310',
             height: 85
@@ -25,10 +55,26 @@ class App extends React.Component {
 
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <StatusBar barStyle="light-content" />
-                <Button type="primary">Test</Button>
-            </View>
+            <ThemeProvider theme={theme}>
+                <ScrollView>
+                    <StatusBar barStyle="light-content"/>
+                    <Card>
+                        <Text style={{fontWeight: '500', fontSize: 18}}>Carbon Counter</Text>
+                        <Text style={{marginBottom: 10}}>
+                           This will ask you a few questions, calculate your carbon emissions, and give you steps on how you can reduce it.
+                        </Text>
+                        <Button
+                            icon={ <Icon name="arrow-forward" size={20} color="white" iconStyle={{marginTop: 2}}/> }
+                            iconRight
+                            buttonStyle={{marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            title='OPEN '/>
+                    </Card>
+                    {/*<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>*/}
+                    {/*    <Button title="Test"/>*/}
+                    {/*    <Slider style={{width: 300}}/>*/}
+                    {/*</View>*/}
+                </ScrollView>
+            </ThemeProvider>
         );
     }
 }
